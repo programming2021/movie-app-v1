fe127a256367bf6538b3d002fbe440c57240a64f
programming2021 <mr.shirani@aol.com>
2022-07-04 03:01:48 +0200

    Header completed resopnsive withh all linked and logo

--- components/Header.js
+++ components/Header.js
@@ -6,14 +6,17 @@ import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons
 
 export default function Header() {
   return (
-      <div>
-          <div className=''>
+      <div className='bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between'>
+          <div className='flex'>
               <HeaderIcon Icon={HomeIcon} title="HOME"/>
               <HeaderIcon Icon={UserIcon} title="ACCOUNT"/>
               <HeaderIcon Icon={PhoneIcon} title="CONTACT"/>
               <HeaderIcon Icon={InformationCircleIcon} title="ABOUT"/>
           </div>   
-          <Image src={"http://assets.stickpng.com/images/613f661716381700041030fc.png"} width={70} height={70}/>
+          <Image className='cursor-pointer active:brightness-110' src={"http://assets.stickpng.com/images/613f661716381700041030fc.png"} width={70} height={70}/>
     </div>
   )
 }
+
+
+ 
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 export default function HeaderIcon({Icon, title}) {
   return (
-      <div>
+      <div className='mx-4 flex flex-col hover:text-white cursor-pointer lg:mx-6 active:text-red-400'>
           <Icon className="h-6" />
-          <p>{title}</p>
+          <p className='text-sm font-bold my-2'>{title}</p>
     </div> 
   )
 }
