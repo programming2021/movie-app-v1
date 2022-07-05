671044c39c1b6663bf45a4923f90de08f0437608
programming2021 <mr.shirani@aol.com>
2022-07-05 05:57:17 +0200

    Navbar section is now complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,19 @@
  
- import React from 'react'
- 
- export default function Navbar() {
+import React from 'react'
+import requests from "../utils/requests"
+import { useRouter } from 'next/router'
+
+
+export default function Navbar() {
+   const router = useRouter()
    return (
-       <div>
-           <h1>Navbar</h1> 
+       <div className='flex justify-center bg-gray-500 text-gray-100 select-none text-xl lg:text-2xl'>
+         {Object.entries(requests).map(([key, { title, url }]) => {
+           return <h2 onClick={() => {router.push(`?genre=${key}`)}} className='m-8 cursor-pointer hover:text-white active:text-red-600' key={key}> {title} </h2>    
+        })
+      }
+      
      </div>
-   )
- }
+   
+   )}
  
---
+++ utils/requests.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+const API_KEY = process.env.API_KEY
+
+export default {
+    fetchTrending: {
+        title: 'Trending',
+        url: `/trending/all/week? api_key=${API_KEY}&language=en-US`   
+    },
+    fetchTopRated: {
+        title: 'Top Rated',
+        url: `/trending/top_rated? api_key=${API_KEY}&language=en-US`   
+    }
+}
